4cd89b381114c3a752cf90bd0cc94aab8a9a953f
Tonino Mendicino <tonino@clickntap.com>
2017-12-12 16:51:04 +0100

    TapFramework
    Added TapFramework as external dependency

--- NeosuranceSDK/Classes/TapRequest.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface TapRequest : NSObject
-
-+(void)requestWithURL:(NSURL*)url completionHandler:(void (^)(NSURLResponse *response, id responseObject, NSError *error))completionHandler;
-
-@end
--- NeosuranceSDK/Classes/TapSound.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface TapSound : NSObject
-
--(void)play:(NSURL*)url;
-
-@end
